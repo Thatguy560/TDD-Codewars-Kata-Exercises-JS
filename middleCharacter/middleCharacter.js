096efe81707096a147d49338e7f3a03c3945df05
Peter Stevens <Peterstevens510@gmail.com>
2020-10-07 16:13:39 +0100

    middle character kata finished

--- middleCharacter/middleCharacter.js
+++ middleCharacter/middleCharacter.js
@@ -1,7 +1,7 @@
-middleCharacter = (middle) => {
-  let wl = middle.length;
-  if (wl % 2 !== 0) {
-    return "i";
-  } else {
-  }
+middleCharacter = (word) => {
+  let middle = word.length / 2;
+  let wl = word.length;
+  return wl % 2 !== 0
+    ? word.slice(middle, middle + 1)
+    : word.slice(middle - 1, middle + 1);
 };
--- middleCharacter/middleCharacterSpec.js
+++ middleCharacter/middleCharacterSpec.js
@@ -1,5 +1,9 @@
 describe("middleCharacter", () => {
   it("will only return one character if the word is odd", () => {
-    expect(middleCharacter("pie")).toEqual("i");
+    expect(middleCharacter("middle")).toEqual("dd");
+  });
+
+  it("Will return the middle two characters if the word is even", () => {
+    expect(middleCharacter("testing")).toEqual("t");
   });
 });
